dbfd6fe8d727d98fc179078a2625ff90bcb0ced0
Chim Kan <designium@gmail.com>
2012-08-30 19:35:17 -0400

    Streamlined the generation process to remove the overwrite thing.

--- lib/rails/generators/simple_address_generator/simple_address_generator_generator.rb
+++ lib/rails/generators/simple_address_generator/simple_address_generator_generator.rb
@@ -6,6 +6,7 @@ module SimpleAddressGenerator
       def create_address_model
         generate("model", "address address_line_1:string address_line_2:string city:string state:string postal_code:string country:string telephone_number:string latitude:float longitude:float category_id:integer")
         rake("db:migrate")
+        remove_file "address.rb", "app/models/address.rb"
       end
 
       def generate_everything_else
